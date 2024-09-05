e09937fa7891a8e73a8eb82ddfe2de87145dee33
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-09-05 17:52:21 +0530

    commit

---
+++ components/MeetingModal.tsx
@@ -0,0 +1,66 @@
+import React, { ReactNode } from 'react'
+import { cn } from "@/lib/utils";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import Image from 'next/image';
+
+
+interface MeetingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  className?: string;
+  children?: ReactNode;
+  handleClick?: () => void;
+  buttonText?: string;
+  instantMeeting?: boolean;
+  image?: string;
+  buttonClassName?: string;
+  buttonIcon?: string;
+}
+
+
+const MeetingModal = ({
+  isOpen,
+  onClose,
+  title,
+  className,
+  children,
+  handleClick,
+  buttonText,
+  instantMeeting,
+  image,
+  buttonClassName,
+  buttonIcon,
+}: MeetingModalProps) => {
+  return (
+          <Dialog open= {isOpen} onOpenChange={onClose}>
+        
+        <DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white'>
+          <div className='flex flex-col gap-6'>
+          {image && (
+            <div className="flex justify-center">
+              <Image src={image} alt="image" width={72} height={72} />
+            </div>
+          )}
+
+         <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+            {title}
+          </h1>
+         
+          </div>
+         
+        </DialogContent>
+      </Dialog>
+
+  )
+}
+
+export default MeetingModal
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import HomeCard from "./HomeCard"
 import { useState } from "react"
 import { useRouter } from "next/navigation"
+import MeetingModal from "./MeetingModal"
 
 const MeetingTypeList = () => {
 
@@ -12,6 +13,12 @@ const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState<
   'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
    >(undefined);
+
+   const createMeeting =  () => {
+
+   }
+
+
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
             <HomeCard
@@ -40,6 +47,15 @@ const MeetingTypeList = () => {
         description="Meeting Recordings"
         className="bg-yellow-1"
         handleClick={() => router.push('/recordings')}
+      />
+
+      <MeetingModal
+        isOpen = {meetingState === 'isInstantMeeting'}
+        onClose = {() => setMeetingState(undefined)}
+        title = "Start an instant meeting"
+        className = "text-center"
+        buttonText = "Start Meeting "
+        handleClick = {createMeeting}
       />
 
      
