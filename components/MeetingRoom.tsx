384cad8ed71da875689d6d9330f8147d44a37f4f
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-09-10 09:32:26 +0530

    participant menu

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,4 +1,4 @@
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
 import { cn } from '@/lib/utils';
 
@@ -10,7 +10,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { LayoutList } from 'lucide-react';
+import { LayoutList, Users } from 'lucide-react';
 
 
 type CallLayouttype = 'grid' | 'speaker-left' | 'speaker-right'
@@ -59,15 +59,27 @@ const MeetingRoom = () => {
             <DropdownMenuContent className='border-dark-1 bg-dark-500 text-white '>
               {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item,index) => (
                 <div key = {index}>
-                  <DropdownMenuItem>
+                  <DropdownMenuItem className='cursor-pointer'
+                  onClick={() => {
+                    setLayout(item.toLowerCase() as CallLayouttype)
+                  }}>
                     {item}
                   </DropdownMenuItem>
+                  <DropdownMenuSeparator className='border-dark-1'/>
                 </div>
               ))}
-              <DropdownMenuSeparator />
             
             </DropdownMenuContent>
         </DropdownMenu>
+        <CallStatsButton/>
+        <button onClick={() => setShowParticipants ((prev) => !prev)}>
+         
+         <div className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+            <Users size={20} className='text-white'/>
+         </div>
+
+        </button>
+         
 
       </div>
      </section>
