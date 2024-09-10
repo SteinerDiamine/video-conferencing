60aeecab15317747387143d1c6f7fe6057dac84c
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-09-10 17:41:08 +0530

    personal room

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,12 +1,4 @@
-// import React from 'react'
 
-// const CallList = ({ type }: {type: 'ended' | 'upcoming' | 'recordings'}) => {
-//   return (
-//     <div>CallList</div>
-//   )
-// }
-
-// export default CallList
 
 'use client';
 
